e1a77965cc74c6515cff921ad383fcb9a146d7a7
Nastya Sudnik <issna@yandex.ru>
2022-12-16 21:28:50 +0700

    sprint #2, Final: fix review

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <iostream>
 #include <map>
+#include <numeric>
 #include <set>
 #include <sstream>
 #include <string>
@@ -11,6 +12,7 @@
 using namespace std;
 
 constexpr int MAX_RESULT_DOCUMENT_COUNT = 5;
+constexpr double EPSILON = 1e-6;
 
 string ReadLine()
 {
@@ -78,7 +80,7 @@ public:
 
         sort(matched_documents.begin(), matched_documents.end(),
              [](const Document& lhs, const Document& rhs) {
-                 if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
+                 if (abs(lhs.relevance - rhs.relevance) < EPSILON) {
                      return lhs.rating > rhs.rating;
                  } else {
                      return lhs.relevance > rhs.relevance;
@@ -157,14 +159,7 @@ private:
     }
 
     static int ComputeAverageRating(const vector<int>& ratings) {
-        if (ratings.empty()) {
-            return 0;
-        }
-        int rating_sum = 0;
-        for (const int rating : ratings) {
-            rating_sum += rating;
-        }
-        return rating_sum / static_cast<int>(ratings.size());
+        return accumulate(ratings.begin(), ratings.end(), 0) / static_cast<int>(ratings.size());
     }
 
     struct QueryWord {
